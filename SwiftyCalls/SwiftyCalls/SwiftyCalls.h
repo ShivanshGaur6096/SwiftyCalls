d00b309d3cd2663d14cfd18da3fa6dea7f59b919
Shivansh Gaur <shivansh.gaur@blackngreen.com>
2025-02-05 15:25:00 +0530

    Created a SwiftyCalls Framework

---
+++ SwiftyCalls/SwiftyCalls/SwiftyCalls.h
@@ -0,0 +1,18 @@
+//
+//  SwiftyCalls.h
+//  SwiftyCalls
+//
+//  Created by Shivansh Gaur on 05/02/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyCalls.
+FOUNDATION_EXPORT double SwiftyCallsVersionNumber;
+
+//! Project version string for SwiftyCalls.
+FOUNDATION_EXPORT const unsigned char SwiftyCallsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyCalls/PublicHeader.h>
+
+
---
+++ SwiftyCalls/SwiftyCalls/SwiftyCalls.swift
@@ -0,0 +1,15 @@
+//
+//  SwiftyCalls.swift
+//  SwiftyCalls
+//
+//  Created by Shivansh Gaur on 05/02/25.
+//
+
+import Foundation
+import UIKit
+
+public class SwiftyCalls {
+    public static func addButton(to viewController: UIViewController, _ a: Int, _ b: Int) -> Int {
+        return a + b
+    }
+}
